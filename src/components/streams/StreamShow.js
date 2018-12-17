dec950b0f956f5512123025b5b129cfd914e35ee
rinbo <robin.b@outlook.com>
2018-12-17 22:15:12 +0100

    Hookup videoplayer to a live stream on rmtp server

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -18,6 +18,9 @@ class StreamShow extends React.Component {
   componentDidUpdate() {
     this.buildPlayer();
   }
+  componentWillUnmount = () => {
+    this.player.destroy();
+  };
 
   buildPlayer() {
     if (this.player || !this.props.stream) {
